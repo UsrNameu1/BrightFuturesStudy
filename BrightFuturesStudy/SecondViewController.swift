fb8510608507e677849778a0c55f917b9338fca1
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-05-11 16:59:21 +0900

    Add Project

---
+++ BrightFuturesStudy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BrightFuturesStudy
+//
+//  Created by adachi.yuichi on 2015/05/11.
+//  Copyright (c) 2015年 yad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
